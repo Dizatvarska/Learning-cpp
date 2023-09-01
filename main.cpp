5d150e3f080ff89572b960da9379b756302adeaa
Your Name <you@example.com>
2023-09-01 13:22:38 +0000

    adding cpp2

--- main.cpp
+++ main.cpp
@@ -10,13 +10,13 @@ private:
 public:
     Array() {} // Конструктор без параметрів
 
-    int* Create1DArray(const int rows)
+    int* CreateArray(const int rows)
     {
         int *arr = new int[rows];
         return 	arr;
     }
     
-    void Fill1DArray(int* const arr, int const rows) 
+    void FillArray(int* const arr, int const rows) 
     {
         for (int i = 0; i < rows; i++)
         {
@@ -24,7 +24,7 @@ public:
         }
     }
 
-    void Show1DArray(int* const arr, const int rows)
+    void ShowArray(int* const arr, const int rows)
     {
         for (int i = 0; i < rows; i++)
         {
@@ -39,30 +39,30 @@ public:
     }
     // ******************************************
 
-    int** Create2DArray(const int rows, const int cols)
+    int** CreateArray(const int rows, const int cols)
     {
         int** arr = new int* [rows];
         for (int i = 0; i < rows; i++)
         {
-            arr[i] = Create1DArray(cols);
+            arr[i] = CreateArray(cols);
         }
         return arr;
     }
     
-    void Fill2DArray(int** const arr, const int rows, const int cols) 
+    void FillArray(int** const arr, const int rows, const int cols) 
     {
         for (int i = 0; i < rows; i++)
         {
-            Fill1DArray(arr[i], cols);
+            FillArray(arr[i], cols);
         }
 
     }
 
-    void Show2DArray(int** const arr, const int rows, const int cols) 
+    void ShowArray(int** const arr, const int rows, const int cols) 
     {
         for (int i = 0; i < rows; i++)
         {
-            Show1DArray(arr[i], cols);
+            ShowArray(arr[i], cols);
         }
         cout << endl;
 
@@ -79,30 +79,30 @@ public:
 
     //************************************************************
 
-    int*** Create3DArray(const int rows, const int cols, const int depth)
+    int*** CreateArray(const int rows, const int cols, const int depth)
     {
         int*** arr = new int** [depth];
         for (int i = 0; i < depth; i++)
         {
-            arr[i] = Create2DArray(rows, cols);
+            arr[i] = CreateArray(rows, cols);
         }
         return arr;
     }
 
-    void Fill3DArray(int*** const arr, const int rows, const int cols, const int depth) 
+    void FillArray(int*** const arr, const int rows, const int cols, const int depth) 
     {
         for (int i = 0; i < depth; i++)
         {
-            Fill2DArray(arr[i], rows, cols);
+            FillArray(arr[i], rows, cols);
         }
 
     }
 
-    void Show3DArray(int*** const arr, const int rows, const int cols, const int depth) 
+    void ShowArray(int*** const arr, const int rows, const int cols, const int depth) 
     {
         for (int i = 0; i < depth; i++)
         {
-            Show2DArray(arr[i], rows, cols);
+            ShowArray(arr[i], rows, cols);
         }
         cout << endl;
     }
@@ -129,23 +129,23 @@ int main()
     cin >> depth;
     cout << "*********************1d array*********************" << endl;
     Array arr;
-    int* arr1 = arr.Create1DArray(rows);
-    arr.Fill1DArray(arr1, rows);
-    arr.Show1DArray(arr1, rows);
+    int* arr1 = arr.CreateArray(rows);
+    arr.FillArray(arr1, rows);
+    arr.ShowArray(arr1, rows);
     arr.Delete1DArray(arr1);
     cout << "*********************1d array*********************" << endl;
 
     cout << "*********************2d array*********************" << endl;
-    int** arr2 = arr.Create2DArray(rows, cols);
-    arr.Fill2DArray(arr2, rows, cols);
-    arr.Show2DArray(arr2, rows, cols);
+    int** arr2 = arr.CreateArray(rows, cols);
+    arr.FillArray(arr2, rows, cols);
+    arr.ShowArray(arr2, rows, cols);
     arr.Delete2DArray(arr2, rows);
     cout << "*********************2d array*********************" << endl;
 
     cout << "*********************3d array*********************" << endl;
-    int*** arr3 = arr.Create3DArray(rows, cols, depth);
-    arr.Fill3DArray(arr3, rows, cols, depth);
-    arr.Show3DArray(arr3, rows, cols, depth);
+    int*** arr3 = arr.CreateArray(rows, cols, depth);
+    arr.FillArray(arr3, rows, cols, depth);
+    arr.ShowArray(arr3, rows, cols, depth);
     arr.Delete3DArray(arr3, rows, cols);
     cout << "*********************3d array*********************" << endl;
     return 0;
